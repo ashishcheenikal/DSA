287e82288c264d46310da1e0e92773c9252a6eba
ashishcheenikal <ashishcheenikal@gmail.com>
2024-07-02 01:15:06 +0530

    Linked List - push, pop, shift, unshift, get, set, insert, remove

---
+++ main.js
@@ -0,0 +1,123 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    if (value) {
+      const newNode = new Node(value);
+      this.head = newNode;
+      this.tail = newNode;
+      this.length = 1;
+    }
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return "LinkedList is empty";
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+      return "Linked List is empty";
+    }
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return "linkedLIst is empty";
+    }
+    let temp = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    temp.next = null;
+    return temp;
+  }
+
+  get(index) {
+    if (index < 0 || index > this.length) return undefined;
+    let temp = this.head;
+    for (i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+
+  set(index, value) {
+    let temp = this.get(index);
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+    const newNode = new Node(value);
+    let pre = this.get(index - 1);
+    newNode.next = pre.next;
+    pre.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index > this.length) return false;
+    let pre = this.get(index - 1);
+    let ele = pre.next;
+    pre.next = ele.next;
+    ele.next = null;
+    this.length--;
+    return this;
+  }
+  reverse() {}
+}
+
+const linkedList = new LinkedList();
+window.linkedList = linkedList;
